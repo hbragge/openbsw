7568b90837d026a4c5a577229c49eb6a34acc00a
hbragge <hbragge@users.noreply.github.com>
2025-07-31 15:05:34 +0000

    deploy: 3eadf67881c4f6b50a6fc7d892f0d9f23ed66eee

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,77 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"",
+"d1/d38/structasync_1_1internal_1_1TimerTask_3_01Adapter_00_010U_01_4.html",
+"d2/d05/classcan_1_1AbstractStaticBitFieldFilter.html",
+"d3/d35/LifecycleManagerInitializer_8h.html#a81e38dc908a9b611286b404a9336f39d",
+"d4/d78/classuds_1_1OutgoingDiagConnection.html#ac8dafe71dac2add6780db671ba6e6e7f",
+"d5/dc1/IFilteredCANFrameSentListener_8h.html",
+"d7/d5d/IRunnable_8h_source.html",
+"d8/d99/structsafety_1_1Mpu_1_1Descriptor.html#a24fcf1eddacc63ab3e34feea6bd8cdd9",
+"d9/d86/classuds_1_1CommunicationControl.html#a96147174116f2a56efecfbb992332317",
+"da/db8/classuds_1_1ICommunicationStateListener.html#a538e134f6ac395429643f58194103407a84b797e49315f8c2ce77263211525d0b",
+"db/dc2/classuds_1_1IncomingDiagConnection.html#a2d471b9e59aed153b710e6f2b517142d",
+"dc/d64/DoCanMessageReceiveProtocolHandler_8h.html#a9b9169f04760afe57c9c6544f633e6e6ae133e9a63fd4950f19c2180be4047ecc",
+"dd/d1e/classdocan_1_1DoCanReceiver.html",
+"de/d5d/classdocan_1_1IDoCanAddressConverter.html#a4465d1437c4a98f7332dff8c4d8e3878",
+"df/d6f/classutil_1_1defer_1_1Defer.html#a4d30ad081c8e5dd0703933e4fccd2bf5"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
